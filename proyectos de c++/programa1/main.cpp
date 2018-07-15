52470cfdcd3366729deea1b427a518c627df3947
juan <weqe15729@gmail.com>
2018-07-15 17:12:09 -0500

    c++

---
+++ proyectos de c++/programa1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int numero1;
+int numero2;
+int total;
+
+int main()
+{
+    cout <<"digite numero: ";
+    cin  >> numero1;
+
+    cout << "digite numero:\n";
+    cin >>numero2;
+
+    total = numero1 + numero2;
+
+    cout <<"resultado suma de:" << total << endl;
+
+    return 0;
+}
